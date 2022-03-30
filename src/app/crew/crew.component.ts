ec353f5ab9849959448cd6261245f3e0f0894f95
MattRoski <mattroski88@gmail.com>
2022-03-29 22:09:46 -0500

    finished studio w/o bonus

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,9 +13,35 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //always "do things by how its compiled.."
+  //class variables are always declared first
+  //constructors are always RAN first
+  //ngOnInit is ran when everything has been con structed
+  //then anything else matters after that
+  
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
   }
 
 }
